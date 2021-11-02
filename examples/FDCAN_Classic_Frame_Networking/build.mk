54db64d75a27f9a414976a117e91bff0c0b24311
Bengt Marten Agren <marangisto@me.com>
2021-11-02 13:45:25 +0800

    Initial batch of files copied.

---
+++ examples/FDCAN_Classic_Frame_Networking/build.mk
@@ -0,0 +1,7 @@
+MCU=STM32H743ZITx
+BASE_DIR=../../..
+LIBS=corona CubeH7
+DEFS=USE_HAL_DRIVER STM32H743xx USE_STM32H743I_EVAL
+#INCLUDE=../Inc ../../../../../../Drivers/STM32H7xx_HAL_Driver/Inc ../../../../../../Drivers/CMSIS/Device/ST/STM32H7xx/Include ../../../../../../Drivers/CMSIS/Include ../../../../../../Drivers/BSP/STM32H743I-EVAL
+LINK=STM32H743XIHx_FLASH.ld
+ENTRY=Reset_Handler
